1e91baabc93abd97539208aec02d83efb08a2ff3
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-14 00:07:23 +0530

    added footer and header component in router

--- src/router/router.js
+++ src/router/router.js
@@ -1,18 +1,25 @@
 import { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import App from "../App";
+
+//pages
 import AllBlogs from "../pages/AllBlogs";
 import DisplayBlog from "../pages/DisplayBlog";
+//components
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 class Router extends Component {
   render() {
     return (
       <div>
         <BrowserRouter>
+          <Header />
           <Switch>
             <Route path="/blogs" exact component={AllBlogs} />
             <Route path="/blogs/:id" exact component={DisplayBlog} />
           </Switch>
         </BrowserRouter>
+        <Footer />
       </div>
     );
   }
