0f365e9ce8e10d1ddb521ea96adeeb3d91039419
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-13 22:40:30 +0530

    added class to display blog by id

---
+++ src/pages/DisplayBlog.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import url from "../components/ApiCall";
+
+class DisplayBlog extends Component {
+  render() {
+    return (
+      <div>
+        <h1>display Blog</h1>
+      </div>
+    );
+  }
+}
+export default DisplayBlog;
