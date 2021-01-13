ed2673de5f24d73ecd3be01eea4c0f1670f16132
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-13 22:50:14 +0530

    included markup component in display blog

--- src/pages/DisplayBlog.jsx
+++ src/pages/DisplayBlog.jsx
@@ -1,11 +1,12 @@
 import { Component } from "react";
 import url from "../components/ApiCall";
-
+import Blog from "../components/Blog";
 class DisplayBlog extends Component {
   render() {
     return (
       <div>
         <h1>display Blog</h1>
+        <Blog />
       </div>
     );
   }
