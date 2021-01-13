41eeb1f454e48895487c8ccf3c758db58aa8cf6d
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-13 22:35:19 +0530

    added component class to all  blogs

---
+++ src/components/AllBlogCards.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import url from "./ApiCall";
+
+class AllBlogCards extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+export default AllBlogCards;
