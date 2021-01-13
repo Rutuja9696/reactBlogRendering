28f3934971c3e9815ebbfefad65963036a2c1b80
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-13 22:36:36 +0530

    included component in all blogs page

--- src/pages/AllBlogs.jsx
+++ src/pages/AllBlogs.jsx
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
 import url from "../components/ApiCall";
-
+import AllBlogCards from "../components/AllBlogCards";
 class AllBlogs extends Component {
   state = {
     blogs: [],
@@ -11,6 +11,7 @@ class AllBlogs extends Component {
     return (
       <div>
         <h1>all blogs</h1>
+        <AllBlogCards />
       </div>
     );
   }
