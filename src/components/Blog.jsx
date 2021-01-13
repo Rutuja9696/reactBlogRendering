38233975bb17ff276fef9ecb2028d84ab1e42081
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-13 22:45:32 +0530

    added layout for blog

---
+++ src/components/Blog.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class Blog extends Component {
+  render() {
+    return (
+      <div>
+        <h1></h1>
+        <h2></h2>
+        <img />
+        <p></p>
+        <div></div>
+      </div>
+    );
+  }
+}
+export default Blog;
