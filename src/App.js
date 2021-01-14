9c62c1cce52026bc4eac2437be9d9b3f37f0bce4
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-14 22:53:48 +0530

    removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import "./App.css";
-import Router from "./router/router";
 function App() {
-  return (
-    <div>
-      <Router />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
